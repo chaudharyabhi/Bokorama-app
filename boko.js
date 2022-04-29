beccec914a5bf3275b6b9638ffc85af42824b5a3
Abhishek <abhishekchaudhary234@gmail.com>
2022-04-30 01:28:00 +0530

    Added functionality of adding Books

--- boko.js
+++ boko.js
@@ -1,19 +1,41 @@
 
 // deleting items
-var bookName = document.querySelectorAll('.delete');
 
-bookName.forEach(function (book) {
-    book.addEventListener('click', function (e) {
-        let li = book.parentElement;
-        li.style.display = 'none';
-    });
+var bookList = document.querySelector('.book-list');
+
+bookList.addEventListener('click', function (e) {
+    if (e.target.textContent == "delete") {
+
+        e.target.parentElement.style.display = "none";
+    };
 });
 
 // preventing default action of form(add-book)
 
 var addbook = document.querySelector('#add-book');
-addbook.addEventListener('click', function (e) {
+
+
+addbook.addEventListener('submit', function (e) {
     e.preventDefault();
+
+    const newLi = document.createElement('li');
+    const newBook = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+    
+    const value = addbook.querySelector('input').value; 
+
+    deleteBtn.textContent = "delete";
+    newBook.textContent = value;
+
+    deleteBtn.classList.add("delete");
+    newBook.classList.add("title");
+
+    newLi.appendChild(newBook);
+    newLi.appendChild(deleteBtn);
+
+    const ul = document.querySelector('.book-list');
+
+    ul.appendChild(newLi);
 });
 
 
